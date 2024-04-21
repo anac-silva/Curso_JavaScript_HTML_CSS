dd44c020ed0cf1c6f79f5dc8a89e1ff3e174387a
anac-silva <ana.carolina3551@gmail.com>
2024-04-21 09:48:33 -0300

    separado os arquivos

---
+++ Exercicio 8/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minuto}!`;
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotodia.png';
+        document.body.style.background = "#f1cb46";
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'fototarde.png';
+        document.body.style.background = "#e36036";
+    } else {
+        img.src = "fotonoite.png";
+        document.body.style.background = "#312927";
+    }
+}
